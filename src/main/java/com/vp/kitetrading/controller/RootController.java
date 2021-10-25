7064a3b9a3675ab5d4e47bc655d197a89aef7f43
vinayprabhu19 <vinayprabhu19@gmail.com>
2021-10-25 19:20:33 +0530

    key

--- src/main/java/com/vp/kitetrading/controller/RootController.java
+++ src/main/java/com/vp/kitetrading/controller/RootController.java
@@ -1,5 +1,6 @@
 package com.vp.kitetrading.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RootController {
 	
+	@Value("${api_key}")
+	private String apiKey; 
+	
 	@RequestMapping(method = RequestMethod.GET,value = "/")
 	public String root() {
 		return "App is up and running";
+	}
+	
+	@RequestMapping(method = RequestMethod.GET,value = "/key")
+	public String key() {
+		return apiKey;
 	}
 
 }
